364a9d00de2af2968f1b28499ac9e89768298b5e
Carel <careld@voxtelecom.co.za>
2014-11-07 15:04:50 +0200

    initial commit

---
+++ ScreenScrape/MatchSite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ScreenScrape
+{
+    public class MatchSite
+    {
+        //public string get
+
+        public Dictionary<string, string> GetLinks(string url)
+        {
+            Dictionary<string, string> urlList = new Dictionary<string, string>();
+            // make an object of the WebClient class 
+            WebClient objWebClient = new WebClient();
+            // gets the HTML from the url written in the textbox
+            var aRequestHTML = objWebClient.DownloadData(url);
+            // creates UTf8 encoding object
+            UTF8Encoding utf8 = new UTF8Encoding();
+            // gets the UTF8 encoding of all the html we got in aRequestHTML
+            var inputString = utf8.GetString(aRequestHTML);
+            // this is a regular expression to check for the urls
+            string HRefPattern = "href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))";
+            var match = Regex.Match(inputString, HRefPattern,
+                     RegexOptions.IgnoreCase | RegexOptions.Compiled,
+                     TimeSpan.FromSeconds(1));
+
+            // get all the matches depending upon the regular expression
+            var urlListString = "";
+            while (match.Success)
+            {
+                if (!urlList.Keys.Contains(match.Groups[1].ToString()))
+                {
+                    urlList.Add(match.Groups[1].ToString(), match.Groups[1].ToString());
+                    Console.WriteLine("Found href " + match.Groups[1] + " at " + match.Groups[1].Index);
+                    urlListString += match.Groups[1].ToString() + "\n";
+                }
+                match = match.NextMatch();
+            }
+            return urlList;
+        }
+
+        public string GetEntireSiteLinks(string siteName)
+        {
+            return "true";
+        }
+    }
+}
---
+++ ScreenScrape/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ScreenScrape
+{
+    class Program
+    {
+        
+        private static List<string> a = new List<string>();
+        static void Main(string[] args)
+        {
+            MatchSite matchSite = new MatchSite();
+            var linkList = matchSite.GetLinks("http://www.picknpay.co.za/");
+            
+            // The following lines of code writes the extracted Urls to the file named test.txt
+            StreamWriter sw = new StreamWriter(System.IO.Path.GetFullPath("test.txt"));
+            sw.Write(urlListString);
+            sw.Close(); 
+            Console.ReadLine();
+        }
+    }
+}
